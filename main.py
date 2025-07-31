4da8be11aec4ddc58bdedeb9077da4142b49b590
Dillon Chong <124565587+dillonchong01@users.noreply.github.com>
2025-07-31 19:56:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ import models
 import expense_service
 import routes
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+with app.app_context():
+    db.create_all()
